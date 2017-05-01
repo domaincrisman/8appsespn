65002ba5b609b44982db1afac8b700c7896315f0
domaincrisman <cmanzanore@gmail.com>
2017-05-01 23:39:17 +0200

    Add Comments to Message View File

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
 	private
 
 	def comment_params
-		params.require(:comments).permit(:content)
+		params.require(:comment).permit(:content)
 	end
 
 end
